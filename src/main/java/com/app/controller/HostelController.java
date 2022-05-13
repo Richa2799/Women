64e1c05a262e60aff55e7ad5b8f744ecd5f24fe4
Paras Bora <parasborarm7@gmail.com>
2022-05-13 10:53:54 +0530

    update hostel path mapping

--- src/main/java/com/app/controller/HostelController.java
+++ src/main/java/com/app/controller/HostelController.java
@@ -3,9 +3,13 @@ package com.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.exception.ResourceNotFoundException;
+import com.app.model.Candidate;
 import com.app.model.Hostel;
 import com.app.service.HostelService;
 
@@ -15,9 +19,8 @@ public class HostelController {
 	HostelService hs;
 	
 	
-	@GetMapping("/getAllHostel")
+	@GetMapping("/Hostel")
 	public List<Hostel> getHostelList(){
 		return hs.HostelList();
 	}
-
 }
