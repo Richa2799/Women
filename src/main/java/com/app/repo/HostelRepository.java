08381f713f5c01aa98f3a7d3ff3b56903f264dea
Paras Bora <parasborarm7@gmail.com>
2022-05-13 15:34:38 +0530

    add hostel module

--- src/main/java/com/app/repo/HostelRepository.java
+++ src/main/java/com/app/repo/HostelRepository.java
@@ -1,10 +1,11 @@
 package com.app.repo;
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.app.model.Hostel;
 
-public interface HostelRepository extends JpaRepository<Hostel,Integer> {
+@Repository
+public interface HostelRepository extends JpaRepository<Hostel,Long> {
 	
 }
